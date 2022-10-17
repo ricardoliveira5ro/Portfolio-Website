e24b3d56a28fbe4c4bc3bc9843407145b580fb89
Ricardo Oliveira <201901561@estudantes.ips.pt>
2022-10-18 00:48:22 +0100

    side bar for small screens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import { IgxNavigationDrawerComponent } from 'igniteui-angular';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css', './media-queries.css']
 })
 export class AppComponent {
+  
 }
