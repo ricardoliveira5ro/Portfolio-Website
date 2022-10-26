fa24e55b1f21e25b676dfa9d8939f2fc7644d676
Ricardo Oliveira <201901561@estudantes.ips.pt>
2022-10-26 11:45:57 +0100

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { IgxNavigationDrawerComponent } from 'igniteui-angular';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css', './media-queries.css']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   
