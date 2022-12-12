fa1bf9773296fcf1df63934b8582478165cb53ae
Ricardo Oliveira <201901561@estudantes.ips.pt>
2022-12-12 17:44:08 +0000

    project section complete fix temporary routes home nav

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../models/project.class';
+import { ProjectsDataService } from '../services/projects-data.service';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css', './media-queries.css']
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  projectsList: Project[];
+  nextSection: string;
+
+  constructor(private service: ProjectsDataService) {
+    this.projectsList = this.service.getProjectsList();
+    this.nextSection = "About"
+  }
 
   ngOnInit(): void {
   }
