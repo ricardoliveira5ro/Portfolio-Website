d6042728b4874f498f6ef064fda78d0921f35c9b
Ricardo Oliveira <201901561@estudantes.ips.pt>
2023-01-23 16:26:48 +0000

    rebuild & integrate about section

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import * as moment from 'moment';
+import { FadeIn } from '../app.routing.animation';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css', './media-queries.css']
+  styleUrls: ['./about.component.css', './media-queries.css'],
+  animations: [FadeIn(500, true)]
 })
 export class AboutComponent implements OnInit {
 
-  nextSection: string;
-
-  constructor() { 
-    this.nextSection = "Contacts"
+  constructor(private router: Router) { 
   }
 
   ngOnInit(): void {
@@ -19,6 +19,14 @@ export class AboutComponent implements OnInit {
 
   getAge() {
     return moment().diff(new Date(2001, 6, 28), 'years');
+  }
+
+  nextSection() {
+    this.router.navigate(['about-me']);
+  }
+
+  home() {
+    this.router.navigate(['']);
   }
 
 }
