9d844b8b08cda2f87d75e0a457226fd91ad021a7
Ricardo Oliveira <201901561@estudantes.ips.pt>
2023-02-01 23:25:12 +0000

    .gitignore is now working

--- src/app/models/technology.class.ts
+++
@@ -1,9 +0,0 @@
-export class Technology {
-    title: string;
-    imageURL: string;
-
-    constructor(title: string, imageURL: string) {
-        this.title = title;
-        this.imageURL = imageURL;
-    }
-}
